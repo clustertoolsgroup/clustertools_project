e66973785491e662932795465538603f43b80c06
mattes <mm.91@hotmail.de>
2017-06-19 13:12:17 +0200

    Spectral clustering cleanup, update example notebook

--- clustertools/models/graph.py
+++ clustertools/models/graph.py
@@ -10,13 +10,15 @@ from clustertools.models.distance import KMeans
 
 class SpectralClustering(object):
 
-    def __init__(self,data,k,similarity_metric='eps_dist',laplacian='standard',metric='euclidean',
-                 low_dim_clustering = None,verbose=True,**kwargs):
+    def __init__(self, data, k, similarity_measure='gaussian', laplacian='normalized', metric='euclidean',
+                 low_dim_clustering = None, verbose=True, **kwargs):
         '''
-        Graph-based Spectral Clustering
+        Graph-based Spectral Clustering, normalized cuts algorithm by default (normalized laplacian matrix),
+	see https://people.eecs.berkeley.edu/~malik/papers/SM-ncut.pdf
+	
         Args:
             data: (n,d)-shaped two-dimensional ndarray
-            similarity metric: specification of similarity measure on data
+            similarity measure: specification of similarity measure on data
                 'eps_dist' for discrete minimal distance measure: optional argument 'eps' must be passed
                 'gaussian' for gaussian similarity measure: optional argument 'bandwidth' must be passed
             metric: specification of used metric, see scipy.spatial.distance docs
@@ -33,7 +35,7 @@ class SpectralClustering(object):
         self._data = data
         self._k = k
         self._laplacian = laplacian
-        self._similarity_metric = similarity_metric
+        self._similarity_measure = similarity_measure
         self._cluster_labels = None
         self._n_clusters = None
         self._verbose = verbose
@@ -45,10 +47,10 @@ class SpectralClustering(object):
         if self._metric != 'euclidean':
             print('Warning: spectral clustering initialized with different metric than euclidean metric.')
             
-        if self._similarity_metric == 'eps_dist' and self._eps is None:
+        if self._similarity_measure == 'eps_dist' and self._eps is None:
             raise TypeError('Argument "eps" for given similarity metric not found')
             
-        if self._similarity_metric == 'gaussian' and self._bandwidth is None:
+        if self._similarity_measure == 'gaussian' and self._bandwidth is None:
             raise TypeError('Argument "bandwidth" for given similarity metric not found')
         
 
@@ -67,29 +69,26 @@ class SpectralClustering(object):
         '''
 
         if self._verbose:
-            start_time = timer()
+       	    start_time = timer()
 
-        # initialize variables
         [n_samples,dim] = self._data.shape
-        #visited = np.zeros(n_samples,dtype=bool)
         cluster_labels = [None]*n_samples
-        #cluster_index = 0
-        #noise_counter = 0
-
+        
         #----------------------------------------------------
         
         #compute adjacency array
         distances = distance.cdist(self._data,self._data,metric=self._metric)
-        if self._similarity_metric == 'eps_dist':
+        if self._similarity_measure == 'eps_dist':
             print('Constructing discrete similarity matrix')
             W = (distances < self._eps).astype(int)
-        if self._similarity_metric == 'gaussian':
+        if self._similarity_measure == 'gaussian':
             print('Constructing gaussian similarity matrix')
             W = np.exp(-1*np.power(distances,2)/(2*self._bandwidth*2))
             
-        
+        #graph degree matrix
         D = np.diag(np.sum(W,axis=0))
         
+		#construct laplacian matrix
         if self._laplacian == 'standard':
             print('Computing standard Laplacian eigenproblem')
             L = D-W
@@ -99,23 +98,27 @@ class SpectralClustering(object):
             L = D-W
             eigvals,eigvecs = eig(L,D)
             
-        #if self._low_dim_clustering is None:
-        # self.low_dim_clustering = KMeans(verbose = False)
-            
+        #rescaling of eigenvectors for computational purpose       
         eigvecs = dim*(eigvecs)[:,0:self._k]
         
         
-        cluster_obj = KMeans(data = eigvecs,k=self._k,method='kmeans++',max_iter=300,atol=10**-12,rtol=10**-12)
+		#------------------
+		#lowdimensional KMeans run on eigenvectors
+
+		#if self._verbose:
+		#	print('\n')
+		#	print('KMeans initialization on eigenvectors...')
+	
+		#TODO optional KMeans arguments for clustering on reduced eigenspace
+        cluster_obj = KMeans(data = eigvecs,k=self._k,method='kmeans++',max_iter=300,atol=10**-12,rtol=10**-12,verbose=self._verbose)
         cluster_obj.fit()
         labels = cluster_obj._cluster_labels
         self.cluster_labels = labels
-
+	
+		#main algorithm verbosity
         if self._verbose:
-            #print('Detected %i clusters'%cluster_index)
+			#print ('KMeans terminated. \n')
             elapsed_time = timer() - start_time
             elapsed_time = timedelta(seconds=elapsed_time)
             print('Finished after ' + str(elapsed_time))
-            #noise_rate = noise_counter/n_samples
-            #print('Rate of noise in dataset: %f'%noise_rate)
-        #self._n_clusters = cluster_index
-
+          
